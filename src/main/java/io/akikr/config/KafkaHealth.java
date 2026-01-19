90f5861b832b2918855da2a0de0a8ea21b143ac7
ankit <akikr@duck.com>
2026-01-19 22:52:39 +0530

    Add: The custom HealthIndicator for kafka

---
+++ src/main/java/io/akikr/config/KafkaHealth.java
@@ -0,0 +1,45 @@
+package io.akikr.config;
+
+import org.apache.kafka.clients.admin.AdminClient;
+import org.apache.kafka.clients.admin.AdminClientConfig;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.actuate.health.Health;
+import org.springframework.boot.actuate.health.HealthIndicator;
+import org.springframework.stereotype.Component;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.TimeoutException;
+
+@Component("kafka")
+public class KafkaHealth implements HealthIndicator {
+
+    @Value("${spring.kafka.consumer.bootstrap-servers:localhost:9092}")
+    private String consumerBootstrapServers;
+
+    @Value("${spring.kafka.producer.bootstrap-servers:localhost:9092}")
+    private String producerBootstrapServers;
+
+    @Override
+    public Health health() {
+        Map<String, Object> configs = new HashMap<>();
+        String bootstrapServers = consumerBootstrapServers + "," + producerBootstrapServers;
+        configs.put(AdminClientConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
+
+        try (AdminClient client = AdminClient.create(configs)) {
+            client.describeCluster()
+                    .clusterId()
+                    .get(5L, TimeUnit.SECONDS);
+
+            return Health.up()
+                    .withDetail("consumerBootstrapServers", consumerBootstrapServers)
+                    .withDetail("producerBootstrapServers", producerBootstrapServers)
+                    .build();
+
+        } catch (InterruptedException | ExecutionException | TimeoutException e) {
+            return Health.down(e).build();
+        }
+    }
+}
